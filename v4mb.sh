e3ac4821981a34e683c068c7f08a5328f818a315
Leo Wu <65029966+WLy2431217633@users.noreply.github.com>
2021-06-05 17:25:01 +0800

    Update v4mb.sh

--- v4mb.sh
+++ v4mb.sh
@@ -1,4 +1,4 @@
-wget https://ghproxy.com/https://raw.githubusercontent.com/WLy2431217633/MianBan/blob/main/v4mb.tar.gz
+wget https://ghproxy.com/https://raw.githubusercontent.com/WLy2431217633/MianBan/main/v4mb.tar.gz
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
 mv v4mb/panel panel
